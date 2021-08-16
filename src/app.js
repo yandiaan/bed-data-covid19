c0d535350d5b7d95911d72d1015d66da5f98a410
Dian Setiawan <diansetiawan2121@gmail.com>
2021-08-17 01:06:22 +0700

    ajax complete

--- src/app.js
+++ src/app.js
@@ -3,5 +3,6 @@ const axios = require("axios");
 import "./styles/main.scss";
 import "popper.js";
 import "bootstrap/dist/js/bootstrap";
+
 import "./script/main";
 // document.addEventListener("DOMContentLoaded", main);
--- src/script/main.js
+++ src/script/main.js
@@ -4,29 +4,21 @@ import "./components/nav-bar.js";
 let url = "https://rs-bed-covid-api.vercel.app/api";
 let province = document.getElementById("province");
 let cities = document.getElementById("cities");
-
-// fetch(url, {
-//   method: "get",
-//   dataType: "json",
-//   headers: {
-//     Accept: "application/json",
-//     "Content-Type": "application/json",
-//   },
-// })
-//   .then((response) => response.json())
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(`error : ${error}`));
+let cov = document.getElementById("cov");
+let dataContainer = document.getElementById("dataContainer");
+let btnSearch = document.getElementById("btnSearch");
 
 axios.get(`${url}/get-provinces`).then((response) => {
   let data = response.data.provinces;
   data.forEach((result) => {
     province.innerHTML += `
-    <option value="${result.id}">${result.name}</option>
-    `;
+              <option value="${result.id}">${result.name}</option>
+              `;
   });
 });
 
 province.addEventListener("change", () => {
+  cities.innerHTML = `<option selected disabled hidden>Pilih Kabupaten/Kota</option>`;
   cities.removeAttribute("disabled");
   axios
     .get(`${url}/get-cities?provinceid=${province.value}`)
@@ -34,14 +26,33 @@ province.addEventListener("change", () => {
       let data = response.data.cities;
       data.forEach((result) => {
         cities.innerHTML += `
-        <option value="${result.id}">${result.name}</option>
-        `;
+          <option value="${result.id}">${result.name}</option>
+          `;
       });
     });
 });
 
-let welcome = document.getElementById("welcome");
+cities.addEventListener("change", () => {
+  cov.removeAttribute("disabled");
+});
 
-welcome.addEventListener("click", () => {
-  welcome.classList.add("disappear");
+btnSearch.addEventListener("click", () => {
+  dataContainer.innerHTML = `<h1 class="text-center">Data Rumah Sakit</h1>`;
+  axios
+    .get(
+      `${url}/get-hospitals?provinceid=${province.value}&cityid=${cities.value}&type=${cov.value}`
+    )
+    .then((response) => {
+      let data = response.data.hospitals;
+      data.forEach((result) => {
+        dataContainer.innerHTML += `<div class="card bg-dark text-light w-75 mx-auto mt-4 mb-2 p-3 text-center rounded">
+          <p>Nama Rumah Sakit : ${result.name}</p>
+          <p>Alamat : ${result.address}</p>
+          <p>Telepon : ${result.phone}</p>
+          <p>Antrian : ${result.queue}</p>
+          <p>Kamar Kosong : ${result.bed_availability}</p>
+          <p>${result.info}</p>
+        </div>`;
+      });
+    });
 });
